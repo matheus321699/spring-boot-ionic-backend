1619f32d2f32448a7f333fc183747cfab5a8a7af
Matheus Henrique <matheus260400@gmail.com>
2021-11-11 15:03:06 -0300

    Produto e associacao muitos para muitos

---
+++ src/main/java/com/github/matheus321699/cursomc/repositories/ProdutoRepository.java
@@ -0,0 +1,12 @@
+package com.github.matheus321699.cursomc.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import org.springframework.stereotype.Repository;
+
+import com.github.matheus321699.cursomc.domain.Produto;
+
+@Repository
+public interface ProdutoRepository extends JpaRepository<Produto, Integer>{
+
+}
