90b86ef81775ee850eca601c189e862d0b10007b
Matheus Henrique <matheus260400@gmail.com>
2021-12-14 16:03:44 -0300

    Paginação com parâmetros opcionais na requisição

--- src/main/java/com/github/matheus321699/cursomc/services/CategoriaService.java
+++ src/main/java/com/github/matheus321699/cursomc/services/CategoriaService.java
@@ -5,6 +5,9 @@ import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort.Direction;
 import org.springframework.stereotype.Service;
 
 import com.github.matheus321699.cursomc.domain.Categoria;
@@ -67,6 +70,25 @@ public class CategoriaService {
 	 */
 	public List<Categoria> findAll() {
 		return repo.findAll();
+	}
+	
+	/*
+	 * Criando um serviço de paginação para buscar registros no banco de dados de acordo
+	 * com uma quantidade determinada de regsitros.
+	 */
+	// Page encapsula informações e operações de paginações
+	/*
+	 * Parâmetro page: qual a página
+	 * Parâmetro linesPage: Linhas por página
+	 * Parâmetro orderBy: por qual atributo será ordenado
+	 * Parâmetro : qual direção deve ser ordenado, ascendente ou descendente
+	 */
+	public Page<Categoria> findPage(Integer page, Integer linesPage, String orderBy, String direction){
+		/* pageRequest: Objeto que vai preparar as informações para realizar uma consulta que 
+		* retorne a página de dados
+		*/
+		PageRequest pageRequest = PageRequest.of(page, linesPage, Direction.valueOf(direction), orderBy);
+		return repo.findAll(pageRequest);
 	}
 	
 }
